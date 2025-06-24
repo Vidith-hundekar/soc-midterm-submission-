3401c7bc23a08f6bad00403d59450e256bf1686b
Vidith-hundekar <hundekarvidith@gmail.com>
2025-06-24 14:56:28 +0530

    Add files via upload

---
+++ soc_midterm/soc_submission-1/Ball game_46A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef int64_t ll;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+typedef complex<ll> P;
+ 
+#define X real()
+#define Y imag()
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
+#define sz size
+#define iArray(a, n) for (ll i = 0; i < n; i++) cin >> a[i];
+#define i1(a) cin>>a;
+#define i2(a, b) cin >> a >> b;
+#define o1(a) cout << a << "\n";
+#define o2(a, b) cout << a << ' ' << b << "\n";
+#define oArray(a, n) for (ll i = 0; i < n; i++) cout << a[i] << ' '; cout << endl;
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define all(x) x.begin(), x.end()
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    i1(n)
+    for(int i=1;i<n;i++){
+        if(((1 + ((i*(i+1))/2))%n)!=0)
+        cout<<(1 + ((i*(i+1))/2))%n<<" ";
+        else cout<<n<<" ";
+    }
+    }
